e62a8aae5ca973b4c83aad7511d0d34e026cd672
zero0828 <ching780828@hotmail.com>
2017-06-16 22:21:51 +0800

    按照91的建議進行修改
    1.) 變更測試來源資料的建立方式
    var products = new List<Product>()
    {
       new Product() { Id=1 ,Cost=1 ,Revenue=11,SellPrice=21 },
       new Product() { Id=2 ,Cost=2 ,Revenue=12,SellPrice=22 },
    };
    
    2.) 泛型類別改成泛型方法
    var actual = target.Sum<Product>(products, groupSize, (p) => p.Revenue);
    
    3.) 修改命名空間的名稱
    CalculationByGroupFromCollection = > CalculationFromCollection

--- Day1/CalculationByGroupFromCollection/CalculationByGroupFromCollection/CalculationByGroupFromCollection.cs
+++ Day1/CalculationByGroupFromCollection/CalculationByGroupFromCollection/CalculationByGroupFromCollection.cs
@@ -4,41 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CalculationByGroupFromCollection
+namespace CalculationFromCollection
 {
     /// <summary>
     /// 依照特定條件，計算集合中的資料
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class CalculationByGroupFromCollection<T>
+    public class CalculationByGroupFromCollection
     {
-        public IEnumerable<T> _Collection;
         /// <summary>
         /// 依照特定條件，計算集合中的資料
         /// </summary>
-        /// <param name="Collection">資料集合</param>
-        public CalculationByGroupFromCollection(IEnumerable<T> Collection)
+        public CalculationByGroupFromCollection()
+        {
+        }
+        /// <summary>
+        /// 檢查集合
+        /// <para>當空集合則拋出例外ArgumentException</para>
+        /// </summary>
+        /// <param name="Collection"></param>
+        private void IsCollectionTheNullThrowArgumentException<TSource>(IEnumerable<TSource> Collection)
         {
             if (!Collection.Any()) { throw new ArgumentException("加總的數量小於1"); }
-            this._Collection = Collection;
         }
+
         /// <summary>
         /// 依照從集合中依照順序，進行分組後進行指定項目加總
         /// </summary>
         /// <param name="GroupSize">數量進行分組</param>
         /// <param name="selector">欄位</param>
         /// <returns>加總後的數值</returns>
-        public IEnumerable<int> Sum(int GroupSize, Func<T, int> selector)
+        public IEnumerable<int> Sum<TSource>(IEnumerable<TSource> Collection, int GroupSize, Func<TSource, int> selector)
         {
+            IsCollectionTheNullThrowArgumentException(Collection);
             if (GroupSize <= 0) { throw new ArgumentException("加總的數量小於1"); }
-            
+
             List<int> Sums = new List<int>();
-            
-            foreach (IEnumerable<T> subCollection in GroupBySize(GroupSize))
+
+            foreach (IEnumerable<TSource> subCollection in GroupBySize(Collection, GroupSize))
             {
                 Sums.Add(subCollection.Sum(selector));
-            }
-             
+            } 
             return Sums;
         }
         /// <summary>
@@ -46,17 +51,17 @@ namespace CalculationByGroupFromCollection
         /// </summary>
         /// <param name="Size">一組數量</param>
         /// <returns>一個集合中包含多個子集</returns>
-        private IEnumerable<IEnumerable<T>> GroupBySize(int Size)
+        private IEnumerable<IEnumerable<TSource>> GroupBySize<TSource>(IEnumerable<TSource> Collection, int Size)
         {
-            List<List<T>> Collections = new List<List<T>>();
-            int SubCollectionCount = this._Collection.Count() / Size;
-            if ((this._Collection.Count() / Size) != 0) { SubCollectionCount++; }
+            List<List<TSource>> Collections = new List<List<TSource>>();
+            int SubCollectionCount = Collection.Count() / Size;
+            if ((Collection.Count() / Size) != 0) { SubCollectionCount++; }
             for (int count = 0; count < SubCollectionCount; count++)
             {
-                Collections.Add(new List<T>());                 
+                Collections.Add(new List<TSource>());
             }
-            List<T> Items = this._Collection.ToList();
-            for (int count = 0; count < this._Collection.Count(); count++)
+            List<TSource> Items = Collection.ToList();
+            for (int count = 0; count < Collection.Count(); count++)
             {
                 int Index = count / Size;
                 Collections[Index].Add(Items[count]);
--- Day1/CalculationByGroupFromCollection/CalculationByGroupFromCollectionTests/CalculationByGroupFromCollectionTests.cs
+++ Day1/CalculationByGroupFromCollection/CalculationByGroupFromCollectionTests/CalculationByGroupFromCollectionTests.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CalculationByGroupFromCollection;
+using CalculationFromCollection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FluentAssertions;
 using ExpectedObjects;
 
-namespace CalculationByGroupFromCollection.Tests
+namespace CalculationFromCollection.Tests
 {
     [TestClass()]
     public class CalculationByGroupFromCollectionTests
@@ -19,20 +19,20 @@ namespace CalculationByGroupFromCollection.Tests
         /// <returns>Input</returns>
         private IEnumerable<Product> StubProducts()
         {
-            IList<Product> Products = new List<Product>();
-
-            Products.Add(new Product() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
-            Products.Add(new Product() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
-            Products.Add(new Product() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
-            Products.Add(new Product() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
-            Products.Add(new Product() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
-            Products.Add(new Product() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
-            Products.Add(new Product() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
-            Products.Add(new Product() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
-            Products.Add(new Product() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
-            Products.Add(new Product() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
-            Products.Add(new Product() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
-
+            var Products = new List<Product>()
+            {
+                new Product() { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 },
+                new Product() { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 },
+                new Product() { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 },
+                new Product() { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 },
+                new Product() { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 },
+                new Product() { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 },
+                new Product() { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 },
+                new Product() { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 },
+                new Product() { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 },
+                new Product() { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 },
+                new Product() { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 },
+            };
             return Products;
         }
 
@@ -41,12 +41,12 @@ namespace CalculationByGroupFromCollection.Tests
         {
             //arrange
             var products = StubProducts();
-            var target = new CalculationByGroupFromCollection<Product>(products);
+            var target = new CalculationByGroupFromCollection();
 
             var expected = new List<int> { 6, 15, 24, 21 };
-            int GroupSize = 3;
+            int groupSize = 3;
             //act
-            var actual = target.Sum(GroupSize, (p) => p.Cost);
+            var actual = target.Sum<Product>(products, groupSize, (p) => p.Cost);
 
             //assert
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -57,13 +57,13 @@ namespace CalculationByGroupFromCollection.Tests
         {
             //arrange
             var products = StubProducts();
-            var target = new CalculationByGroupFromCollection<Product>(products);
+            var target = new CalculationByGroupFromCollection();
 
 
             var expected = new List<int> { 50, 66, 60 };
-            int GroupSize = 4;
+            int groupSize = 4;
             //act
-            var actual = target.Sum(GroupSize, (p) => p.Revenue);
+            var actual = target.Sum<Product>(products, groupSize, (p) => p.Revenue);
 
             //assert
             expected.ToExpectedObject().ShouldEqual(actual);
@@ -74,12 +74,12 @@ namespace CalculationByGroupFromCollection.Tests
         {
             //arrange
             var products = StubProducts();
-            var target = new CalculationByGroupFromCollection<Product>(products);
+            var target = new CalculationByGroupFromCollection();
             var expected = new int[] { 50, 66, 60 };
 
-            int GroupSize = 0;
+            int groupSize = 0;
             //act
-            Action actual = () => target.Sum(GroupSize, (p) => p.Revenue);
+            Action actual = () => target.Sum<Product>(products, groupSize, (p) => p.Revenue);
 
             //assert 
             actual.ShouldThrow<ArgumentException>();
@@ -87,20 +87,20 @@ namespace CalculationByGroupFromCollection.Tests
 
         [TestMethod()]
         public void SumTest_輸入GroupSize為負數_預期會拋ArgumentException()
-        { 
+        {
             //arrange
             var products = StubProducts();
-            var target = new CalculationByGroupFromCollection<Product>(products);
+            var target = new CalculationByGroupFromCollection();
             var expected = new int[] { 50, 66, 60 };
 
-            int GroupSize = -1;
+            int groupSize = -1;
             //act
-            Action actual = () => target.Sum(GroupSize, (p) => p.Revenue);
+            Action actual = () => target.Sum<Product>(products, groupSize, (p) => p.Revenue);
 
             //assert 
             actual.ShouldThrow<ArgumentException>();
         }
-        
+
         //讓編譯器處理
         //[TestMethod()]
         //public void SumTest_當集合中加總的欄位不存在_預期會拋ArgumentException()
@@ -111,15 +111,15 @@ namespace CalculationByGroupFromCollection.Tests
 
 
         //    var expected = new int[] { 50, 66, 60 };
-        //    int GroupSize = 4;
+        //    int groupSize = 4;
 
         //    //act
-        //    Action actual = () => target.Sum(GroupSize, (p) => p.Id);
+        //    Action actual = () => target.Sum(groupSize, (p) => p.Id);
 
         //    //assert 
         //    actual.ShouldThrow<ArgumentException>();
         //}
-        
+
         /// <summary>
         /// 測試時使用的假型別
         /// </summary>
